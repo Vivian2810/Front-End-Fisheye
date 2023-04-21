c6ae603f508df296617fcdc57c87e3e505879c29
Vivian28 <vivian.michel@orange.fr>
2023-04-21 10:44:20 +0200

    added photo add display photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, cost} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -8,9 +8,23 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const location = document.createElement( 'p' );
+        const description = document.createElement( 'p' );
+        const price = document.createElement( 'p' );
+        const div = document.createElement( 'div' );
         h2.textContent = name;
+        location.textContent = city + ', ' + country;
+        location.classList.add('location');
+        description.textContent = tagline;
+        description.classList.add('description');
+        price.textContent = cost + '€/jour';
+        price.classList.add('price');
+        div.appendChild(location);
+        div.appendChild(description);
+        div.appendChild(price);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(div);
         return (article);
     }
     return { name, picture, getUserCardDOM }
