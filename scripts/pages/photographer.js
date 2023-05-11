8ababc1aa0680d130231e68e353b835bd11dbdc1
Vivian28 <vivian.michel@orange.fr>
2023-05-11 15:41:26 +0200

    ajout des likes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,7 +1,8 @@
 //Mettre le code JavaScript lié à la page photographer.html
-
 const logo = document.querySelector(".logo");
+let totalLikes = 0;
 
+// fonction de récupération des données du photographe
 async function getPhotographer() {
   const id = window.location.search.split("=")[1];
   const dataJson = await fetch("/data/photographers.json").then((Response) =>
@@ -13,13 +14,13 @@ async function getPhotographer() {
     (photographer) => photographer.id == id
   );
   const medias = media.filter((media) => media.photographerId == id);
-  console.log(photographer, medias);
   return {
     photographer,
     medias,
   };
 }
 
+// fonction pour afficher les informations du photographe
 async function displayPhotographer(photographer) {
   const { name, city, country, tagline, price, portrait } = photographer;
   const photographerSection = document.querySelector(".photograph-header");
@@ -39,6 +40,7 @@ async function displayPhotographer(photographer) {
   photographerSection.append(photographImg);
 }
 
+// fonction pour afficher les photos et les vidéos
 async function displayPhoto(media, photographer) {
   photographer_name = photographer.name.split(" ")[0].split("-")[0];
   const listImage = document.querySelector(".list-image");
@@ -46,6 +48,7 @@ async function displayPhoto(media, photographer) {
     const { video, image, title, likes, id, price } = m;
     let article = "";
     let count = likes;
+    totalLikes += likes;
     if (video) {
       article = `
         <article id="${id}">
@@ -54,9 +57,9 @@ async function displayPhoto(media, photographer) {
           </video>
           <div class="info">
             <h2>${title}</h2>
-            <div class="like">
+            <div class="like" id="${id}">
               <p>${count}</p>
-              <i class="fas fa-heart"></i>
+              <i class="far fa-heart"></i>
             </div>
           </div>
         </article>
@@ -69,38 +72,48 @@ async function displayPhoto(media, photographer) {
             <h2>${title}</h2>
             <div class="like">
               <p>${count}</p>
-              <i class="fas fa-heart"></i>
+              <i class="far fa-heart"></i>
             </div>
           </div>
         </article>
         `;
     }
     listImage.innerHTML += article;
-        
-    document.querySelectorAll(".like").addEventListener("click", (heart) => {
-      if (heart.target.classList.contains("fas")) {
-        count--;
-        heart.target.classList.remove("fas");
-        heart.target.classList.add("far");
-        // cardsMediaCompteurLike.textContent = media.likes;
-        // displayInfo();
-      } else {
-        count++;
-        heart.target.classList.remove("far");
-        heart.target.classList.add("fas");
-        // cardsMediaCompteurLike.textContent = media.likes;
-        // displayInfo();
-      }
+    document.querySelectorAll(".like").forEach((e) => {
+      e.addEventListener("click", () => {
+        e.classList.toggle("liked");
+        let count = parseInt(e.children[0].innerHTML);
+        e.classList.contains("liked") ? (count++, totalLikes++) : (count--, totalLikes--);
+        e.children[0].innerHTML = count;
+        e.children[0].classList.remove("liked");
+        e.children[1].classList.toggle("far");
+        e.children[1].classList.toggle("fas");
+        displayTotalLikes(totalLikes, photographer);
+      });
     });
   });
-  // console.log(article);
 }
 
+//fonction pour afficher le nombre total de likes
+function displayTotalLikes(totalLikes, photographer) {
+  const totalLikesSection = document.querySelector(".total-likes");
+  totalLikesSection.innerHTML = `
+    <div class="like">
+      <p>${totalLikes}</p>
+      <i class="fas fa-heart"></i>
+    </div>
+    <div>
+      <p>${photographer.price}€ / jour</p>
+    </div>
+  `;
+}
+
+// fonction d'initialisation
 async function init() {
   const { photographer, medias } = await getPhotographer();
-  console.log(medias);
   displayPhotographer(photographer);
   displayPhoto(medias, photographer);
+  displayTotalLikes(totalLikes, photographer);
 }
 
 init();
